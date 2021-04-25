fbbab0447f577c7bf490a757221443193f236b7f
Vaneyk1973 <71929186+Vaneyk1973@users.noreply.github.com>
2021-04-25 21:45:45 +0300

    Update HomeController.java

--- demo/src/main/java/com/example2/demo/HomeController.java
+++ demo/src/main/java/com/example2/demo/HomeController.java
@@ -3,6 +3,7 @@ package com.example2.demo;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
 import java.util.Random;
 
 @RestController
@@ -23,5 +24,12 @@ public class HomeController {
     }
 
     @RequestMapping("")
-    public String g(){return "ttt";}
+    public String g(){
+        String html="<html>\n" +
+                "<body>\n" +
+                "<a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a>\n" +
+                "</body>\n" +
+                "</html>";
+        return html;
+    }
 }
