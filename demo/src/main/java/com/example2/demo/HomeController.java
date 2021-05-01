31f91d5035e777bb7a3a92f09866db091aad07f4
Vaneyk1973 <71929186+Vaneyk1973@users.noreply.github.com>
2021-05-01 18:35:56 +0300

    Update HomeController.java

--- demo/src/main/java/com/example2/demo/HomeController.java
+++ demo/src/main/java/com/example2/demo/HomeController.java
@@ -37,4 +37,172 @@ public class HomeController {
                 "</html>";
         return html;
     }
+
+    @RequestMapping("/0")
+    public double g0(){
+        double a=0;
+        while (a<=0)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/0.5")
+    public double g05(){
+        double a=0;
+        while (a<0.5)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/1")
+    public double g1(){
+        double a=0;
+        while (a<1)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/1.5")
+    public double g15(){
+        double a=0;
+        while (a<1.5)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/2")
+    public double g2(){
+        double a=0;
+        while (a<2)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/2.5")
+    public double g25(){
+        double a=0;
+        while (a<2.5)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/3")
+    public double g3(){
+        double a=0;
+        while (a<3)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/3.5")
+    public double g35(){
+        double a=0;
+        while (a<3.5)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/4")
+    public double g4(){
+        double a=0;
+        while (a<4)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/4.5")
+    public double g45(){
+        double a=0;
+        while (a<4.5)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/5")
+    public double g5(){
+        double a=0;
+        while (a<5)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
+
+    @RequestMapping("/5.5")
+    public double g55(){
+        double a=0;
+        while (a<5.5)
+            a=new Random().nextGaussian();
+        /*String html="<html>\n" +
+                "<body>\n" +
+                "<p><a href=\"https://m5hw.herokuapp.com/index\"><button>JJJJ</button></a></p>\n" +
+                "<image src=\"https://st4.depositphotos.com/16030310/25211/v/1600/depositphotos_252112804-stock-illustration-vector-illustration-of-golden-letters.jpg\"/>\n" +
+                "</body>\n" +
+                "</html>";*/
+        return a;
+    }
 }
