9a278da5e5c875483ed3695f03a30ed902ac6117
Vaneyk1973 <71929186+Vaneyk1973@users.noreply.github.com>
2021-05-29 20:10:40 +0300

    Update HomeController.java

--- demo/src/main/java/com/example2/demo/HomeController.java
+++ demo/src/main/java/com/example2/demo/HomeController.java
@@ -18,6 +18,8 @@ public class HomeController {
 
     @RequestMapping("/put_message")
     public String put_message(@RequestParam("message") String message){
+        if (message==null)
+            return new Gson().toJson("LLL");
         messages.add(new Gson().fromJson(message, Message.class));
         return new Gson().toJson(new Gson().fromJson(message, Message.class).message);
     }
